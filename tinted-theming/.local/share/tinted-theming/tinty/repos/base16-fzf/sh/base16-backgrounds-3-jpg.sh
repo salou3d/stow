76ec641b872ef7c408600d24e1fe62fe11d30e95
Sal Oued <salo@localhost.localdomain>
2024-08-28 19:39:55 +0100

    Adding tinted-theming configs

---
+++ tinted-theming/.local/share/tinted-theming/tinty/repos/base16-fzf/sh/base16-backgrounds-3-jpg.sh
@@ -0,0 +1,9 @@
+# Scheme name: backgrounds-3-jpg
+# Scheme system: base16
+# Scheme author: Tinty
+# Template author: Tinted Theming (https://github.com/tinted-theming)
+
+export FZF_DEFAULT_OPTS="$FZF_DEFAULT_OPTS"\
+" --color=bg+:#425253,bg:#263a3d,spinner:#3ccdc8,hl:#085cba"\
+" --color=fg:#b3b2a9,header:#f7a345,info:#dd6a64,pointer:#3ccdc8"\
+" --color=marker:#3ccdc8,fg+:#d0cabf,prompt:#dd6a64,hl+:#085cba"
---
+++ tinted-theming/.local/share/tinted-theming/tinty/repos/base16-fzf/sh/base16-nature-7-jpg.sh
@@ -0,0 +1,9 @@
+# Scheme name: nature-7-jpg
+# Scheme system: base16
+# Scheme author: Tinty
+# Template author: Tinted Theming (https://github.com/tinted-theming)
+
+export FZF_DEFAULT_OPTS="$FZF_DEFAULT_OPTS"\
+" --color=bg+:#685c56,bg:#4e3f39,spinner:#23c3c3,hl:#1f36da"\
+" --color=fg:#d1cfc8,header:#addc57,info:#98a749,pointer:#23c3c3"\
+" --color=marker:#23c3c3,fg+:#ecece5,prompt:#98a749,hl+:#1f36da"
