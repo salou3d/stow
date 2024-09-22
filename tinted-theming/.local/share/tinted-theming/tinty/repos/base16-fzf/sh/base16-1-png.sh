1fc00e60efd2768d8ffb3e54ed151e51e8a826fa
Sal Oued <salo@localhost.localdomain>
2024-09-22 23:27:06 +0100

    stowing: amp, app, fselect, helix, ianny, rebos, ripgrep-all, scout

---
+++ tinted-theming/.local/share/tinted-theming/tinty/repos/base16-fzf/sh/base16-1-png.sh
@@ -0,0 +1,9 @@
+# Scheme name: 1-png
+# Scheme system: base16
+# Scheme author: Tinty
+# Template author: Tinted Theming (https://github.com/tinted-theming)
+
+export FZF_DEFAULT_OPTS="$FZF_DEFAULT_OPTS"\
+" --color=bg+:#425357,bg:#273d44,spinner:#14ecd4,hl:#1738c3"\
+" --color=fg:#aeada3,header:#edb47d,info:#d99070,pointer:#14ecd4"\
+" --color=marker:#14ecd4,fg+:#c9c4b6,prompt:#d99070,hl+:#1738c3"
---
+++ tinted-theming/.local/share/tinted-theming/tinty/repos/base16-fzf/sh/base24-tinty-generated.sh
@@ -0,0 +1,9 @@
+# base24-Tinty Generated
+# Scheme author: Tinty
+# Template author: Tinted Theming (https://github.com/tinted-theming)
+
+export FZF_DEFAULT_OPTS=" \
+ --color=bg:#34231d,fg:#b7b2ab,hl:#98a749\
+ --color=bg+:#685c56,fg+:#b7b2ab,hl+:#7bbc73\
+ --color=info:#5a375e,border:#1f36da,prompt:#5de34d\
+ --color=pointer:#34231d,marker:#a3ba78,spinner:#a3ba78,header:#7a554a"
