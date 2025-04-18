0fead6879e086d876167514795630f0c882873ec
Sal Oued <yahyaidir@gmail.com>
2025-04-18 13:34:08 +0100

    update fuzzel, micro and walker

---
+++ tinted-theming/.local/share/tinted-theming/tinty/repos/base16-fzf/sh/base16-minimal-14-jpg.sh
@@ -0,0 +1,9 @@
+# Scheme name: minimal-14-jpg
+# Scheme system: base16
+# Scheme author: Tinty
+# Template author: Tinted Theming (https://github.com/tinted-theming)
+
+export FZF_DEFAULT_OPTS="$FZF_DEFAULT_OPTS"\
+" --color=bg+:#5c5554,bg:#453d39,spinner:#5e90b2,hl:#003b90"\
+" --color=fg:#b7b5c0,header:#ffc46f,info:#f2ad64,pointer:#5e90b2"\
+" --color=marker:#5e90b2,fg+:#cecedb,prompt:#f2ad64,hl+:#003b90"
