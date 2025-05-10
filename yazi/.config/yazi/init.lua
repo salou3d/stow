1f27345986d416df2a400e049007c37df541cafd
Sal Oued <yahyaidir@gmail.com>
2025-05-10 01:19:44 +0100

    fixing wleave not showing icons, updating yazi plugins and adding new flavors

--- yazi/.config/yazi/init.lua
+++ yazi/.config/yazi/init.lua
@@ -7,3 +7,12 @@ require("full-border"):setup {
 require("smart-enter"):setup {
 	open_multi = true,
 }
+
+-- require("simple-mtpfs"):setup({})
+
+-- -- In yazi.toml change linemode = "permissions_and_size"
+-- function Linemode:permissions_and_size()
+-- 	local size = self._file:size()
+-- 	local perms = self._file.cha:perm()
+-- 	return string.format("%s %s", perms, size and ya.readable_size(size) or "-")
+-- end
